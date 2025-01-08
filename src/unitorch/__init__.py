b2000ac2eda598a61064e284115484b69341d482
fuliucansheng <fuliucansheng@gmail.com>
2025-01-08 06:55:15 -0800

    update version

--- src/unitorch/__init__.py
+++ src/unitorch/__init__.py
@@ -37,7 +37,7 @@ def mktempfile(prefix: str = "", suffix: str = ""):
 
 
 ### version
-VERSION = "0.0.0.22"
+VERSION = "0.0.0.23"
 
 ### is offline mode
 UNITORCH_OFFLINE = os.environ.get("UNITORCH_OFFLINE", "0").upper()
