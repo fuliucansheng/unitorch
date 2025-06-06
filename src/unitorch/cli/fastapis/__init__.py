c64c40faed2d92eff3106af7ca8b36caed4ae555
fuliucansheng <fuliucansheng@gmail.com>
2025-06-06 11:03:22 -0700

    fix flux tuning

--- src/unitorch/cli/fastapis/__init__.py
+++ src/unitorch/cli/fastapis/__init__.py
@@ -1,15 +1,18 @@
 # Copyright (c) FULIUCANSHENG.
 # Licensed under the MIT License.
 
+from unitorch.utils import is_diffusers_available
 import unitorch.cli.fastapis.info
 import unitorch.cli.fastapis.bria
 import unitorch.cli.fastapis.llava
-import unitorch.cli.fastapis.stable
-import unitorch.cli.fastapis.controlnet
-import unitorch.cli.fastapis.stable_xl
-import unitorch.cli.fastapis.controlnet_xl
-import unitorch.cli.fastapis.stable_3
-import unitorch.cli.fastapis.controlnet_3
-import unitorch.cli.fastapis.stable_flux
-import unitorch.cli.fastapis.controlnet_flux
-import unitorch.cli.fastapis.wan
+
+if is_diffusers_available():
+    import unitorch.cli.fastapis.stable
+    import unitorch.cli.fastapis.controlnet
+    import unitorch.cli.fastapis.stable_xl
+    import unitorch.cli.fastapis.controlnet_xl
+    import unitorch.cli.fastapis.stable_3
+    import unitorch.cli.fastapis.controlnet_3
+    import unitorch.cli.fastapis.stable_flux
+    import unitorch.cli.fastapis.controlnet_flux
+    import unitorch.cli.fastapis.wan
