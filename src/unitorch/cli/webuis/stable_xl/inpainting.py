ad788e87a142df293b28935400351e270574b421
fuliucansheng <fuliucansheng@gmail.com>
2025-06-07 01:10:02 -0700

    fix webui

--- src/unitorch/cli/webuis/stable_xl/inpainting.py
+++ src/unitorch/cli/webuis/stable_xl/inpainting.py
@@ -221,7 +221,7 @@ class StableXLImageInpaintingWebUI(SimpleWebUI):
             trigger_mode="once",
         )
         image.change(
-            lambda x: x["background"].size,
+            lambda x: x["background"].size if x is not None else (1024, 1024),
             inputs=[image],
             outputs=[width, height],
         )
@@ -258,6 +258,8 @@ class StableXLImageInpaintingWebUI(SimpleWebUI):
         return self._status
 
     def composite_images(self, images):
+        if images is None or "layers" not in images:
+            return None
         layers = images["layers"]
         if len(layers) == 0:
             return None
