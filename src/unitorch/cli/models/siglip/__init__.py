859b268e7f82dcf1cc2832f181f6d7a2130e15f7
decu <decu@microsoft.com>
2026-05-06 08:34:49 +0000

    update siglip pretrain

--- src/unitorch/cli/models/siglip/__init__.py
+++ src/unitorch/cli/models/siglip/__init__.py
@@ -75,6 +75,20 @@ pretrained_siglip_infos = {
             "/google/siglip2-so400m-patch16-512/resolve/main/model.safetensors"
         ),
     },
+    "siglip2-large-patch16-512": {
+        "config": hf_endpoint_url(
+            "/google/siglip2-large-patch16-512/resolve/main/config.json"
+        ),
+        "vision_config": hf_endpoint_url(
+            "/google/siglip2-large-patch16-512/resolve/main/preprocessor_config.json"
+        ),
+        "vocab": hf_endpoint_url(
+            "/google/siglip2-large-patch16-512/resolve/main/tokenizer.model"
+        ),
+        "weight": hf_endpoint_url(
+            "/google/siglip2-large-patch16-512/resolve/main/model.safetensors"
+        ),
+    },
 }
 
 pretrained_siglip_extensions_infos = {}
